ae5f98d6ed88a8babb906774512acd599190e719
mordegardor <vladkucher3196@gmail.com>
2016-12-17 14:04:09 +0200

    HW01_optional

--- src/main/java/ru/javawebinar/topjava/dao/MealDao.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDao.java
@@ -5,20 +5,44 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MealDao {
-    private List<Meal> meals = Arrays.asList(
-            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
-            );
 
-    public List<Meal> list(){
-        return meals;
+    private Map<Integer, Meal> meals = new ConcurrentHashMap<>();
+
+    {
+        addMeal(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        addMeal(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        addMeal(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        addMeal(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        addMeal(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        addMeal(new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
+    }
+
+
+
+    public void addMeal(Meal meal) {
+        meals.put(meal.getId(),meal);
+    }
+
+    public void deleteMeal(int Id) {
+        meals.remove(Id);
+    }
+
+    public void updateMeal(Meal meal) {
+        meals.remove(meal.getId());
+        meals.put(meal.getId(),meal);
+    }
+
+    public List<Meal> getAllMeals() {
+        return new CopyOnWriteArrayList<>(meals.values());
+    }
+
+    public Meal getMealById(int Id) {
+        return meals.get(Id);
     }
 }
