453241249fd90ad2945fd2c8e9e0a752ef90e7c7
mordegardor <vladkucher3196@gmail.com>
2017-02-15 10:46:08 +0200

    9_02_HW8_clear_filter

--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -2,6 +2,11 @@ var ajaxUrl = "ajax/profile/meals/";
 var datatableApi;
 
 // $(document).ready(function () {
+function clearFilter() {
+    $("#filter")[0].reset();
+    $.get(ajaxUrl, updateTableByData);
+}
+
 function updateTable() {
     $.ajax({
         type: "POST",
