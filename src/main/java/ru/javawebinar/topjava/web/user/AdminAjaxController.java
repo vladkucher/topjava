97898fcbb5f68b94bbf7bab58b2d3d5981f8238f
mordegardor <vladkucher3196@gmail.com>
2017-03-04 16:28:31 +0200

    11_07_JsonView

--- src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminAjaxController.java
@@ -3,8 +3,10 @@ package ru.javawebinar.topjava.web.user;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.View;
 import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,12 +20,14 @@ public class AdminAjaxController extends AbstractUserController {
 
     @Override
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @JsonView(View.UI.class)
     public List<User> getAll() {
         return super.getAll();
     }
 
     @Override
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @JsonView(View.UI.class)
     public User get(@PathVariable("id") int id) {
         return super.get(id);
     }
